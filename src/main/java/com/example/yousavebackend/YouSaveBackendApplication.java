37c721ed13baa95ffd022c5be44a8d27cdec4719
Maryem khaoua <maryem.khaoua@gmail.com>
2024-11-29 09:51:08 +0100

    first commit

---
+++ src/main/java/com/example/yousavebackend/YouSaveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.yousavebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouSaveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouSaveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yousavebackend/YouSaveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yousavebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouSaveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
