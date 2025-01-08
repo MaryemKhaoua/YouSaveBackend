389fdbf2e3561a44e805605e89c3ff6d4a0ce4f7
Maryem khaoua <maryem.khaoua@gmail.com>
2025-01-08 10:56:52 +0100

    controller for auth

--- src/main/java/com/example/yousavebackend/Controllers/AuthController.java
+++ src/main/java/com/example/yousavebackend/Controllers/AuthController.java
@@ -1,23 +1,24 @@
 package com.example.yousavebackend.Controllers;
 
-import com.example.yousavebackend.DTOs.*;
+import com.example.yousavebackend.DTOs.AuthRequestDTO;
+import com.example.yousavebackend.DTOs.JwtResponseDTO;
+import com.example.yousavebackend.DTOs.RegisterRequestDTO;
 import com.example.yousavebackend.entities.User;
 import com.example.yousavebackend.security.JwtService;
 import com.example.yousavebackend.services.IUserService;
-import com.example.yousavebackend.services.implementations.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
 
 @RestController
-public class AurhController {
+public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
     @Autowired
@@ -26,25 +27,22 @@ public class AurhController {
     @Qualifier("userServiceImpl")
     IUserService userService;
 
-
     @PostMapping("/auth/login")
-    public JwtResponseDTO AuthenticateAndGetToken(@RequestBody AuthRequestDTO authRequestDTO){
-        System.out.println("rah dkhelt");
+    public JwtResponseDTO authenticateAndGetToken(@RequestBody AuthRequestDTO authRequestDTO) {
         Authentication authentication =
                 authenticationManager.authenticate(
                         new UsernamePasswordAuthenticationToken(authRequestDTO.getEmail(),
                                 authRequestDTO.getPassword()));
-        if(authentication.isAuthenticated()){
+        if (authentication.isAuthenticated()) {
             return JwtResponseDTO.builder()
                     .accessToken(jwtService.GenerateToken(authRequestDTO.getEmail())).build();
         } else {
-            throw new UsernameNotFoundException("invalid user request..!!");
+            throw new UsernameNotFoundException("Invalid user request..!!");
         }
     }
 
     @PostMapping("/auth/register")
     public ResponseEntity<User> register(@RequestBody RegisterRequestDTO registerRequestDTO) throws Exception {
-        System.out.println("rah dkhelt");
-        return ResponseEntity.ok (userService.register (registerRequestDTO));
+        return ResponseEntity.ok(userService.register(registerRequestDTO));
     }
 }
